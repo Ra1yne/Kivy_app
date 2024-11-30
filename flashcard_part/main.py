646a13133cfdd57c5b11dabc60d181805b1af991
Ra1yne <1hchuazhijie@gmail.com>
2024-11-30 23:34:57 +0800

    change

--- flashcard_part/main.py
+++ flashcard_part/main.py
@@ -1,47 +1,47 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 # Flashcard Screen Logic
 class FlashcardScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.flashcards = []  # To store flashcards as a list of dictionaries
-        self.current_index = 0  # Track the current flashcard
+        self.flashcards = []
+        self.current_index = 0
 
     def add_flashcard(self, question, answer):
-        """Add a new flashcard."""
         if question and answer:
             self.flashcards.append({"question": question, "answer": answer})
             self.ids.flashcard_question.text = "Flashcard added!"
+            self.ids.flashcard_answer.text = ""
         else:
-            self.ids.flashcard_question.text = "Please enter both question and answer!"
+            self.ids.flashcard_question.text = "Please enter both question and answer."
 
     def show_flashcard(self):
-        """Display the current flashcard's question."""
         if self.flashcards:
             card = self.flashcards[self.current_index]
             self.ids.flashcard_question.text = card["question"]
             self.ids.flashcard_answer.text = ""
+        else:
+            self.ids.flashcard_question.text = "No flashcards available."
 
     def flip_flashcard(self):
-        """Show the answer to the current flashcard."""
         if self.flashcards:
             card = self.flashcards[self.current_index]
             self.ids.flashcard_answer.text = card["answer"]
+        else:
+            self.ids.flashcard_answer.text = "No flashcards to flip."
 
     def next_flashcard(self):
-        """Move to the next flashcard."""
         if self.flashcards:
             self.current_index = (self.current_index + 1) % len(self.flashcards)
             self.show_flashcard()
 
-# Main App
-class FlashcardApp(App):
+class QuizFlashcardApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(FlashcardScreen(name="flashcard"))
         return sm
 
 if __name__ == "__main__":
-    FlashcardApp().run()
+    QuizFlashcardApp().run()
+
